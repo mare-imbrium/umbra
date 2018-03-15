cf7359debbf9a97fa76885f4a98450fa6f6116af
kepler <githubkepler.50s@gishpuppy.com>
2018-03-15 22:59:22 +0530

    add age. add alert

--- ex2.rb
+++ ex2.rb
@@ -5,6 +5,17 @@ require './window.rb'
 require './label.rb'
 require './field.rb'
 
+def create_footer_window h = 2 , w = FFI::NCurses.COLS, t = FFI::NCurses.LINES-2, l = 0
+  ewin = Window.new(h, w , t, l)
+end
+def _alert str 
+  win = create_footer_window
+  win.wbkgd(FFI::NCurses.COLOR_PAIR(12))
+  win.printstring(0,1, str)
+  win.wrefresh
+  win.getkey
+  win.destroy
+end
 def startup
   require 'logger'
   require 'date'
@@ -23,6 +34,7 @@ end
 begin
   init_curses
   startup
+  FFI::NCurses.init_pair(12,  COLOR_WHITE, FFI::NCurses::RED)
   win = Window.new
   statusline(win, " "*(win.width-0), 0)
   statusline(win, "Press q to quit #{win.height}:#{win.width}", 20)
@@ -31,20 +43,21 @@ begin
 
   form = Form.new win
   form.add_widget title
-  labels = ["Name: ", "Address: ","Mobile No.", "Email Id:","Hobbies: "]
+  labels = ["Name:", "Age:", "Address:","Mobile No.:", "Email Id:","Hobbies:"]
   labs = []
   row = 3
   col = 5
   labels.each_with_index {|lab, ix| 
-    w = Label.new( :text => lab, :row => row, :col => col )
+    w = Label.new( :text => lab, :row => row, :col => col , :width => 20)
     labs << w
     row += 2
     w.color_pair = 1
+    w.justify = :right
     w.attr = FFI::NCurses::A_BOLD
     form.add_widget w
   }
   labels = ["Roger Federer", "20 Slam Drive","9810012345", "ihumble@tennis.com","golf, programming"]
-  labels = ["name", "address","mobile", "email","hobbies"]
+  labels = ["name", "age", "address","mobile", "email","hobbies"]
  
   row = 3
   col = 30
@@ -55,12 +68,17 @@ begin
     row += 2
     w.color_pair = 1
     w.attr = FFI::NCurses::A_REVERSE
+    w.null_allowed = true
     form.add_widget w
   }
   #fhash["mobile"].type = :integer
   fhash["mobile"].chars_allowed = /[\d\-]/
   fhash["mobile"].maxlen = 10
   fhash["email"].chars_allowed = /[\w\+\.\@]/
+  fhash["email"].valid_regex = /\w+\@\w+\.\w+/
+  fhash["age"].valid_range = (18..100)
+  fhash["age"].type = :integer
+  fhash["hobbies"].maxlen = 100
   form.pack
   form.select_first_field
   win.wrefresh
@@ -70,8 +88,9 @@ begin
     begin
       form.handle_key ch
     rescue => e
-      puts e
-      puts e.backtrace.join("\n")
+      _alert(e.to_s)
+      $log.error e
+      $log.error e.backtrace.join("\n")
     end
     win.wrefresh
   end
