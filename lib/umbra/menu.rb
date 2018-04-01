4b6e3463c5ce115e3d21fa8ee7becbffa4056e00
kepler <githubkepler.50s@gishpuppy.com>
2018-04-01 09:46:41 +0530

    add module Umbra

--- lib/umbra/menu.rb
+++ lib/umbra/menu.rb
@@ -4,7 +4,7 @@
 #       Author: j kepler  http://github.com/mare-imbrium/canis/
 #         Date: 2018-03-13
 #      License: MIT
-#  Last update: 2018-03-30 15:54
+#  Last update: 2018-04-01 09:27
 # ----------------------------------------------------------------------------- #
 #  menu.rb  Copyright (C) 2012-2018 j kepler
 
@@ -23,66 +23,68 @@
 # == CHANGELOG
 #
 require 'umbra/window'
-class Menu
+module Umbra
+  class Menu
 
-  def initialize title, hash, config={}
+    def initialize title, hash, config={}
 
-    @list = hash.values
-    @keys = hash.keys.collect { |x| x.to_s }
-    @hash = hash
-    bkgd = config[:bkgd] || FFI::NCurses.COLOR_PAIR(14) | BOLD
-    @attr = BOLD
-    @color_pair = config[:color_pair] || 14
-    ht = @list.size+2
-    wid = config[:width] || 40
-    top = (FFI::NCurses.LINES - ht)/2
-    left = (FFI::NCurses.COLS - wid)/2
-    @window = Window.new(ht, wid, top, left)
-    @window.wbkgd(bkgd)
-    @window.box
-    @window.title(title)
-    @current = 0
-    print_items @hash
-  end
-  def print_items hash
-    ix = 0
-    hash.each_pair {|k, val|
-      attr = @attr
-      attr = REVERSE if ix == @current
-      @window.printstring(ix+1 , 2, "#{k}    #{val}", @color_pair, attr )
-      ix += 1
-    }
-    @window.refresh
-  end
-  def getkey
-    ch = 0
-    char = nil
-    begin
-      while (ch = @window.getkey) != FFI::NCurses::KEY_CTRL_C
-        break if ch == 27 # ESC
-        tmpchar = FFI::NCurses.keyname(ch) rescue '?'
-        if @keys.include? tmpchar
-          char = tmpchar
-          break
-        end
-        case ch
-        when FFI::NCurses::KEY_DOWN
-          @current += 1
-        when FFI::NCurses::KEY_UP
-          @current -= 1
-        when FFI::NCurses::KEY_RETURN
-          char = @keys[@current]
-          break
-        end
-        @current = 0 if @current < 0
-        @current = @list.size-1 if @current >= @list.size
-        print_items @hash
-
-        # trap arrow keys here
-      end
-    ensure
-      @window.destroy
+      @list = hash.values
+      @keys = hash.keys.collect { |x| x.to_s }
+      @hash = hash
+      bkgd = config[:bkgd] || FFI::NCurses.COLOR_PAIR(14) | BOLD
+      @attr = BOLD
+      @color_pair = config[:color_pair] || 14
+      ht = @list.size+2
+      wid = config[:width] || 40
+      top = (FFI::NCurses.LINES - ht)/2
+      left = (FFI::NCurses.COLS - wid)/2
+      @window = Window.new(ht, wid, top, left)
+      @window.wbkgd(bkgd)
+      @window.box
+      @window.title(title)
+      @current = 0
+      print_items @hash
     end
-    return char
+    def print_items hash
+      ix = 0
+      hash.each_pair {|k, val|
+        attr = @attr
+        attr = REVERSE if ix == @current
+        @window.printstring(ix+1 , 2, "#{k}    #{val}", @color_pair, attr )
+        ix += 1
+      }
+      @window.refresh
+    end
+    def getkey
+      ch = 0
+      char = nil
+      begin
+        while (ch = @window.getkey) != FFI::NCurses::KEY_CTRL_C
+          break if ch == 27 # ESC
+          tmpchar = FFI::NCurses.keyname(ch) rescue '?'
+          if @keys.include? tmpchar
+            char = tmpchar
+            break
+          end
+          case ch
+          when FFI::NCurses::KEY_DOWN
+            @current += 1
+          when FFI::NCurses::KEY_UP
+            @current -= 1
+          when FFI::NCurses::KEY_RETURN
+            char = @keys[@current]
+            break
+          end
+          @current = 0 if @current < 0
+          @current = @list.size-1 if @current >= @list.size
+          print_items @hash
+
+          # trap arrow keys here
+        end
+      ensure
+        @window.destroy
+      end
+      return char
+    end
   end
-end
+end # module
