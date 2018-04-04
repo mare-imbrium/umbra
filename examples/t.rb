148c1f377705fc51302338c6f79abc89909425ac
kepler <githubkepler.50s@gishpuppy.com>
2018-04-04 09:48:20 +0530

    start a new column after finishing rows

--- examples/t.rb
+++ examples/t.rb
@@ -6,7 +6,7 @@ require 'umbra/form'
 begin
   include Umbra
   init_curses
-  txt = "Press cursor keys to move window"
+  txt = "Demo of keys. Press various keys to see what value comes"
   win = Window.new
   win.printstr txt
   win.printstr("Press Ctrl-Q to quit", 2, 0)
@@ -16,39 +16,19 @@ begin
   form = Form.new win
 
   ch = 0
-  xx = 1
-  yy = 1
   y = x = 1
   while (ch = win.getkey) != FFI::NCurses::KEY_CTRL_Q
-    #y, x = win.getbegyx(win.getwin)
-    old_y, old_x = y, x
-    #log win, :y, y, :x, x, :ch, ch, keyname(ch)
-    # move 0, 0
-    # addstr "y %d x %d" % [y, x]
-    case ch
-    when FFI::NCurses::KEY_RIGHT
-      x += 1
-    when FFI::NCurses::KEY_LEFT
-      x -= 1
-    when FFI::NCurses::KEY_UP
-      y -= 1
-    when FFI::NCurses::KEY_DOWN
-      y += 1
-    end
     rv = nil
     #rv = win.mvwin(y, x)
   
-    #win.printstr "#{ch.to_s}:#{FFI::NCurses::keyname(ch)}", y, x
     win.printstring y, x, "#{ch.to_s}:#{FFI::NCurses::keyname(ch)}"
     y += 1
-    if rv == FFI::NCurses::ERR
-      # put the window back
-      #rv = mvwin(win, old_y, old_x)
+    # start a new column if we exceed last line
+    if y >= FFI::NCurses.LINES-1
+      y = 1
+      x += 10
     end
 
-    # tell ncurses we want to refresh stdscr (to erase win's frame)
-    #touchwin(stdscr)
-    #refresh
     win.wrefresh
   end
 
