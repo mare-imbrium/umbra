e33d733e978fd03498c4eab8dedbd0c996ffc9a5
kepler <githubkepler.50s@gishpuppy.com>
2018-04-07 12:21:48 +0530

    add alert method

--- lib/umbra.rb
+++ lib/umbra.rb
@@ -4,5 +4,12 @@ require "umbra/window"
 require "umbra/form"
 
 module Umbra
-  # Your code goes here...
+  def alert str, config={}
+    require 'umbra/dialog'
+    title = config[:title] || "Alert"
+    cp    = config[:color_pair] || create_color_pair( COLOR_BLUE, COLOR_WHITE )
+    attr  = config[:attrib]     || NORMAL
+    m = Dialog.new text: str, title: title, window_color_pair: cp, window_attr: attr
+    m.run
+  end
 end
