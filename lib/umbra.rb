b6df0652ff10acdbaaa406bcecb6af2643f8487e
kepler <githubkepler.50s@gishpuppy.com>
2018-04-11 10:35:42 +0530

    add Pad view

--- lib/umbra.rb
+++ lib/umbra.rb
@@ -12,4 +12,14 @@ module Umbra
     m = Dialog.new text: str, title: title, window_color_pair: cp, window_attr: attr
     m.run
   end
+
+  # view an array in a popup window
+  def view array, config={}
+    require 'umbra/pad'
+    title = config[:title] || "Viewer"
+    cp    = config[:color_pair] || create_color_pair( COLOR_BLUE, COLOR_WHITE )
+    attr  = config[:attrib]     || NORMAL
+    m = Pad.new list: array, height: FFI::NCurses.LINES-2, width: FFI::NCurses.COLS-10, title: title, color_pair: cp, attrib: attr
+    m.run
+  end
 end
