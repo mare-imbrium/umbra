4b7b672c038ab6e6b7bdfdff1b7543b8d9615ec0
kepler <githubkepler.50s@gishpuppy.com>
2018-04-18 15:42:43 +0530

    config hash for alert dialog

--- lib/umbra.rb
+++ lib/umbra.rb
@@ -6,10 +6,17 @@ require "umbra/form"
 module Umbra
   def alert str, config={}
     require 'umbra/dialog'
-    title = config[:title] || "Alert"
-    cp    = config[:color_pair] || create_color_pair( COLOR_BLUE, COLOR_WHITE )
-    attr  = config[:attrib]     || NORMAL
-    m = Dialog.new text: str, title: title, window_color_pair: cp, window_attr: attr
+    #title = config[:title] || "Alert"
+    #cp    = config[:color_pair] || create_color_pair( COLOR_BLUE, COLOR_WHITE )
+    #attr  = config[:attrib]     || NORMAL
+
+    config[:text]              ||= str
+    config[:title]             ||= "Alert"
+    config[:window_color_pair] ||=  create_color_pair( COLOR_BLUE, COLOR_WHITE )
+    config[:window_attr]       ||= NORMAL
+    
+    #m = Dialog.new text: str, title: title, window_color_pair: cp, window_attr: attr
+    m = Dialog.new config
     m.run
   end
 
