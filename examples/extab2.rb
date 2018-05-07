cd0a092aa0be55a4f936416ab67d0464fcaae351
kepler <githubkepler.50s@gishpuppy.com>
2018-05-07 15:10:14 +0530

    update to use new table class

--- examples/extab2.rb
+++ examples/extab2.rb
@@ -5,9 +5,9 @@
 #       Author: j kepler  http://github.com/mare-imbrium/canis/
 #         Date: 2018-05-06 - 11:20
 #      License: MIT
-#  Last update: 2018-05-06 12:32
+#  Last update: 2018-05-07 15:04
 # ----------------------------------------------------------------------------- #
-#  YFF Copyright (C) 2012-2018 j kepler
+#  extab2.rb  Copyright (C) 2018 j kepler
 require 'umbra'
 require 'umbra/label'
 require 'umbra/tabular'
@@ -25,7 +25,7 @@ def startup
     $log = Logger.new(path)
     $log.level = Logger::DEBUG
     today = Date.today
-    $log.info "Started demo tabular on #{today}"
+    $log.info "Started demo table on #{today}"
     FFI::NCurses.init_pair(10,  FFI::NCurses::BLACK,   FFI::NCurses::GREEN) # statusline
 end
 def statusline win, str, col = 0
@@ -37,27 +37,27 @@ begin
   startup
   win = Window.new
   statusline(win, " "*(win.width-0), 0)
-  statusline(win, "Press q to quit #{win.height}:#{win.width}", 20)
+  statusline(win, "Press C-q to quit #{win.height}:#{win.width}", 20)
   title = Label.new( :text => "Demo of Table", :row => 0, :col => 0 , :width => FFI::NCurses.COLS-1, 
                     :justify => :center, :color_pair => 0)
 
   form = Form.new win
   form.add_widget title
 
-  box = Box.new row: 2, col: 0, width: 10, height: 7
+  box = Box.new row: 2, col: 0, width: 15, height: 7
 =begin
   t = Tabular.new(['a', 'b'], [1, 2], [3, 4], [5,6])
   lb = Listbox.new list: t.render
 =end
-  table = Table.new(['a', 'b'], [1, 2], [3, 4], [5,6])
-  #table.render
+  #table = Table.new(['a', 'b'], [1, 2], [3, 4], [5,6])
+  table = Table.new(columns: ['a', 'b'], data: [[1, 2], [3, 4], [5,6]])
   box.fill table
-  box1 = Box.new row: box.row + box.height + 0, col: box.col, width: 10, height: 7
-  table1 = Table.new ['a', 'b']
+  box1 = Box.new row: box.row + box.height + 0, col: box.col, width: box.width, height: 7
+  table1 = Table.new columns: ['a', 'b']
+  table1 << [8, 6]
   table1 << [1, 2]
   table1 << [3, 4]
   table1 << [4, 6]
-  table1 << [8, 6]
   table1 << [2, 6]
   table1.y = '| '
   table1.x = '+-'
@@ -75,6 +75,19 @@ begin
       lines.each { |e| t.add_row e.chomp.split '|'   }
     end
     t.tabular.use_separator = false
+    # testing out coloring different rows in different colors depending on status
+    def t._format_color index, state
+      arr = super
+      if index == 0                  ## header
+        arr = @header_color_pair || [ CP_MAGENTA, REVERSE ]
+      elsif data[index-1][1] == "clo"
+        arr = [ CP_BLUE, NORMAL ]
+      elsif self.data[index-1][1] == "ope"
+        arr = [ CP_YELLOW, BOLD ]
+      end
+      arr
+    end
+    #t.column_width 4, 15            ## checking truncate of long data
 
     #t.render
     box2 = Box.new title: "tasks.csv", row: box.row , col: box.col + box.width+1, width: FFI::NCurses.COLS-box.width-1, height: FFI::NCurses.LINES-1-box.row
@@ -88,6 +101,7 @@ begin
 
   table3 = Table.new do
     self.headings = 'User',  'Size', 'Mon', 'Date', 'Time', 'File'
+    self.header_color_pair = [ CP_CYAN, REVERSE ]
     res.each { |e|
       cols = e.split
       next if cols.count < 6
@@ -95,8 +109,23 @@ begin
       cols = cols[0..5] if cols.count > 6
       add_row cols
     }
-    column_width 1, 6
+    column_width 1, 10
+    #column_width 5, 10   ## testing truncate
     align_column 1, :right
+    align_column 3, :right
+  end
+
+  # testing out coloring different columns in different color
+  def table3._print_row(win, row, col, str, index, state)
+    f = str.split('|')
+    attrib = NORMAL
+    attrib = BOLD if index == 0
+    attrib = REVERSE if state == :HIGHLIGHTED
+    f.each_with_index {|c, ix|
+      win.printstring(row, col, c, ix+1, attrib)
+      col += c.length+1
+    }
+
   end
   #table3.render
   #t.y = '| '
