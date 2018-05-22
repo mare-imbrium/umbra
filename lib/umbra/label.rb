eecd7158d5896a093d39e6191edfa9237d6d83fe
kepler <githubkepler.50s@gishpuppy.com>
2018-05-22 23:18:25 +0530

    touched up

--- lib/umbra/label.rb
+++ lib/umbra/label.rb
@@ -5,7 +5,7 @@
 #       Author: j kepler  http://github.com/mare-imbrium/canis/
 #         Date: 2018-03-08 - 14:04
 #      License: MIT
-#  Last update: 2018-05-14 14:33
+#  Last update: 2018-05-22 11:01
 # ----------------------------------------------------------------------------- #
 #  label.rb  Copyright (C) 2018- j kepler
 #
@@ -45,7 +45,9 @@ class Label < Widget
   def repaint
     return unless @repaint_required
     raise "Label row or col is nil #{@row} , #{@col}, #{@text} " if @row.nil? || @col.nil?
-    r,c = rowcol
+    #r,c = rowcol
+    r = self.row
+    c = self.col
     $log.debug "label repaint #{r} #{c} #{@text} "
 
     # value often nil so putting blank, but usually some application error
@@ -55,23 +57,27 @@ class Label < Widget
       value = value.join " "
     end
     # ensure we do not exceed
-    if @width
-      if value.length > @width
-        value = value[0..@width-1]
-      end
-    end
-    len = @width || value.length
+    # ## TODO do this in the format commented on 2018-05-22 - 
+    _width = self.width
+    #if _width
+      #if value.length > _width
+        #value = value[0.._width-1]
+      #end
+    #end
+    len = _width || value.length
     acolor = @color_pair  || 0
-    str = @justify.to_sym == :right ? "%*s" : "%-*s"  # added 2008-12-22 19:05 
+    #str = @justify.to_sym == :right ? "%*s" : "%-*s"  # added 2008-12-22 19:05 
+    str = @justify.to_sym == :right ? "%#{len}.#{len}s" : "%-#{len}.#{len}s"  # added 2008-12-22 19:05 
 
-    #@graphic ||= @form.window
     # clear the area
     @graphic.printstring r, c, " " * len , acolor, @attr
     if @justify.to_sym == :center
-      padding = (@width - value.length)/2
+      padding = (_width - value.length)/2
       value = " "*padding + value + " "*padding # so its cleared if we change it midway
     end
-    @graphic.printstring r, c, str % [len, value], acolor, @attr
+    ## move this into paint_label or something so we can override.
+    # try a block which was passed earlier which gets a string TODO
+    @graphic.printstring r, c, str % [value], acolor, @attr
     if @mnemonic
       ulindex = value.index(@mnemonic) || value.index(@mnemonic.swapcase)
       @graphic.mvchgat(y=r, x=c+ulindex, max=1, BOLD|UNDERLINE, acolor, nil)
@@ -86,10 +92,11 @@ class Label < Widget
     raise "Cannot leave Label"
   end
   # overriding so that label is redrawn, since this is the main property that is used.
-  def text=(_text)
-    @text = _text
-    self.touch
-  end
+  ## 2018-05-21 - we are now using attr_property so not required
+  #def text=(_text)
+    #@text = _text
+    #self.touch
+  #end
   # ADD HERE LABEL
 end # }}}
 end # module
